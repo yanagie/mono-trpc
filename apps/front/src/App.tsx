a24253304c45156a56f8bac1ab606f7b2d4d4f60
poteitoo <1105tigery@gmail.com>
2022-11-30 09:49:51 +0900

    update front ui

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,47 +1,80 @@
-import { useEffect } from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 import { trpc } from "./utils/trpc";
 
 function App() {
-  const { isFetching, isError, data, isFetched } = trpc.getUser.useQuery({
-    userId: 1,
-  });
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const mutation = trpc.createUser.useMutation();
-  // const { isFetching, isError, data, isFetched } = trpc.hello.useQuery({
-  //   text: "client",
-  // });
-  const createNewUser = () => {
-    const salt = Math.random().toString().substring(2, 6);
+
+  const { isError, isInitialLoading, isSuccess, data, error } =
+    trpc.getUser.useQuery({
+      userId: 1,
+    });
+  const createNewUser = (name: string, email: string) => {
     mutation.mutate({
-      email: `poepoe${salt}@gmail.com`,
-      name: `poepoe${salt}`,
+      name,
+      email,
     });
   };
 
-  useEffect(() => {
-    console.log({ mutation });
-  }, [mutation]);
-
-  useEffect(() => {
-    if (isFetched) console.log(data);
-  }, [isFetching, isError, data]);
-
-  if (isError) return <p>Error</p>;
-
-  if (isFetching || !data) return <div>Loading...</div>;
-
   return (
     <div className="App">
-      <p>user with id 1: {data.name}</p>
-      <button onClick={createNewUser}>create user</button>
-      {mutation.isSuccess ? (
-        <p>
-          created user: {mutation.data.id}, {mutation.data.name},
-          {mutation.data.email}
-        </p>
-      ) : (
-        <p>error: {mutation.error?.message}</p>
-      )}
-      {/* <p>{data.greeting}</p> */}
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      {isInitialLoading ? (
+        <p>loading</p>
+      ) : isSuccess ? (
+        <div className="align-start">
+          <p>id: {data?.id}</p>
+          <p>username: {data?.name}</p>
+          <p>email: {data?.email}</p>
+        </div>
+      ) : isError ? (
+        <p>{error.message}</p>
+      ) : null}
+      <div className="divider" />
+
+      <div>
+        <p className="hint">ユーザー名</p>
+        <input
+          type="text"
+          className="w-full"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <p className="hint">メールアドレス</p>
+        <input
+          type="text"
+          className="w-full"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <div className="card">
+          <button onClick={() => createNewUser(username, email)}>
+            create user
+          </button>
+        </div>
+
+        {mutation.isLoading ? (
+          <p>loading</p>
+        ) : mutation.isSuccess ? (
+          <div className="align-start">
+            <p>id: {mutation.data.id}</p>
+            <p>username: {mutation.data.name}</p>
+            <p>email: {mutation.data.email}</p>
+          </div>
+        ) : mutation.isError ? (
+          <p>{mutation.error.data?.code}</p>
+        ) : null}
+      </div>
     </div>
   );
 }
